a7472fe150e9e1f1f5687adcabbf9f0d5b0b8236
Joerez <rezenbiz@gmail.com>
2019-02-04 22:03:15 -0800

    first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'todogular';
+  todoArray = []
+
+  addTodo(value){
+    this.todoArray.push(value)
+    console.log(this.todoArray)
+  }
+
+  deleteItem(todo){
+   for (let i = 0; i <= this.todoArray.length; i++) {
+    if (todo == this.todoArray[i]) {
+     this.todoArray.splice(i,1)
+    }
+   }
+  }
+
+  todoSubmit(value:any){
+     if(value!==""){
+    this.todoArray.push(value.todo)
+     //this.todoForm.reset()
+    }else{
+      alert('Field required **')
+    }
+
+  }
+
+}
